6510a12c88fa6d62589bbec0f4fea7a51e385bfd
sagsrinu <sagsrinu@gmail.com>
2026-03-12 10:15:44 +0530

    Day5-2

---
+++ Day-5-12032026-state-remote-backend/variables.tf
@@ -0,0 +1,27 @@
+variable "dev_ami_id" {
+    description = "Passing value to ami_id"
+    default = ""
+    type = string
+  
+}
+variable "dev_instance_type" {
+    description = "Passing Instance Type"
+    default = ""
+    type = string
+  
+
+  
+}
+
+variable "test_ami_id" {
+    description = "Passing value to ami_id"
+    default = ""
+    type = string
+  
+}
+variable "test_instance_type" {
+    description = "Passing Instance Type"
+    default = ""
+    type = string
+  
+}
