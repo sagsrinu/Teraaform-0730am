a17edb2429010ac5f24ec6581bff46b82405e30d
sagsrinu <sagsrinu@gmail.com>
2026-03-25 07:47:41 +0530

    Day13

---
+++ Day-13-14032026-foreach-count/for_each/main.tf
@@ -0,0 +1,27 @@
+variable "ami_id" {
+    description = "passing values to ami_id"
+    default = "ami-02dfbd4ff395f2a1b"
+    type = string
+  
+}
+variable "instance_type" {
+    description = "passing values to instance_type"
+    default = "t2.micro"
+    type = string
+  
+}
+
+variable "env" {
+    description = "environment name"
+    default =[  "test","prod" ]
+    type = list(string)
+}
+
+resource "aws_instance" "name" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    for_each = toset(var.env) #so here toset is used to convert list to set because for_each only accepts map and set not list
+     tags = {
+        Name = each.key  #so here we are creating 3 instances with different name
+}
+}
